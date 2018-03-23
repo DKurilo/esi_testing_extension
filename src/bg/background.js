19be0be1e34babc15d7dd6dd87b219947271522b
Dima Kurilo <dkurilo@purch.com>
2018-03-23 19:15:12 -0400

    added error indications and support for esi:comment and esi:remove

--- src/bg/background.js
+++ src/bg/background.js
@@ -70,6 +70,10 @@ chrome.webRequest.onBeforeRequest.addListener(
     if (status === 'off') {
       return;
     }
+    browser.browserAction.setIcon({
+      path: "icons/icon48.png",
+      tabId: details.tabId
+    });
     const filter = browser.webRequest.filterResponseData(details.requestId);
     const domainPart = details.url.replace(/([^\/]*\/\/[^\/]*).*/ig, '$1');
     const decoder = new TextDecoder("utf-8");
@@ -89,26 +93,40 @@ chrome.webRequest.onBeforeRequest.addListener(
         return;
       }
       let doc = decoder.decode(docStream, {stream: true});
-      const re = /<esi:include\s[^>]*>/ig;
+      const re = /<esi:[^>]*>/ig;
       const found = doc.match(re);
       if (found !== null && found.length > 0) {
         found.forEach(e => {
-          let src = e.replace(/.*src="([^"]*)".*/i, "$1");
-          if (src.indexOf('://') <= 0) {
-            src = domainPart + src;
-          }
-          const xhr = new XMLHttpRequest();
-          xhr.open("GET", src, false);
-          xhr.send(null);
+          const lcE = e.toLowerCase();
+          console.log(lcE);
+          if (lcE.indexOf('<esi:include') === 0) {
+            let src = e.replace(/.*src="([^"]*)".*/i, "$1");
+            if (src.indexOf('://') <= 0) {
+              src = domainPart + src;
+            }
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", src, false);
+            xhr.send(null);
 
-          const headers = xhr.getAllResponseHeaders().replace(/\\/ig, '\\\\').replace(/"/ig, '\\\"').split(/[\r\n]+/).join('\\n');
-          const script = 'console.log(\"ESI fragment:\\n ' + e.replace(/\\/ig, '\\\\').replace(/"/ig, '\\\"') + '\\nStatus: ' + xhr.status + '\\nHeaders:\\n' + headers + '\");';
-          getRequestInfo(details.tabId).scriptsToLaunch.push(script);
+            const headers = xhr.getAllResponseHeaders().replace(/\\/ig, '\\\\').replace(/"/ig, '\\\"').split(/[\r\n]+/).join('\\n');
+            const script = 'console.log(\"ESI fragment:\\n ' + e.replace(/\\/ig, '\\\\').replace(/"/ig, '\\\"') + '\\nStatus: ' + xhr.status + '\\nHeaders:\\n' + headers + '\");';
+            getRequestInfo(details.tabId).scriptsToLaunch.push(script);
 
-          const docarr = doc.split(e);
-          if (xhr.status === 200) {
-            doc = docarr.join(xhr.responseText);
-          } else {
+            const docarr = doc.split(e);
+            if (xhr.status === 200) {
+              doc = docarr.join(xhr.responseText);
+            } else {
+              doc = docarr.join('');
+              browser.browserAction.setIcon({
+                path: "icons/icon48-err.png",
+                tabId: details.tabId
+              });
+            }
+          } else if (lcE.indexOf('<esi:remove') === 0) {
+            const docarr = doc.split(e);
+            doc = docarr.join('');
+          } else if (lcE.indexOf('<esi:comment') === 0) {
+            const docarr = doc.split(e);
             doc = docarr.join('');
           }
         });
@@ -130,7 +148,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
       return;
     }
     const prefix = this.prefixes.getPrefix(details.url);
-    details.requestHeaders.push({name:"Surrogate-Capability",value: ((prefix ? (prefix + '-') : '') + 'ESI/1.0')});
+    details.requestHeaders.push({name:"Surrogate-Capability",value: ((prefix ? prefix : '') + 'ESI/1.0')});
     return {requestHeaders: details.requestHeaders};
   },
   {urls: ["<all_urls>"]},
